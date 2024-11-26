7b319f122d4853c1da79415d814b2974eb9d3a59
JZ <122569479+gisjz@users.noreply.github.com>
2024-11-26 13:50:33 -0600

    Gis-feature (#1)
    * add windy in weatehr summaries
    
    * add Hail in weather summeries
    
    ---------
    
    Co-authored-by: JZLikeCode <jianhuazhou@tamu.edu>

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hail"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Hail", "Windy"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
