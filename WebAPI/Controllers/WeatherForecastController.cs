5338b1d0e1df9195c95868003a3c6a8caef76386
JZLikeCode <jianhuazhou@tamu.edu>
2025-06-26 15:46:18 -0500

    add snowing

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy", "Boilings", "Drizzling", "Falling", "GitFalling", "Frozen"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy", "Boilings", "Drizzling", "Falling", "GitFalling", "Frozen", "Snowing"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
