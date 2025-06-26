a58b424808b8a4de37123e86c891c3e6a34093f7
JZLikeCode <139393342+JZLikeCode@users.noreply.github.com>
2025-06-26 11:14:01 -0500

    Add humid from GitHub editor

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy", "Boilings", "Drizzling", "Frozen"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Windy", "Boilings", "Drizzling", "Frozen", "Humid"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
